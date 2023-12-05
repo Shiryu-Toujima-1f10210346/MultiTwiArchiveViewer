906569368bf51c81212d7aff3340068d093c034f
Shiryu-Toujima-1f10210346 <s1f102103460@iniad.org>
2023-12-05 11:48:08 +0900

    アナリティクス メタデータ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,6 +15,8 @@ import {
 import Image from "next/image";
 import Link from "next/link";
 import { useTweets } from "@/hooks/useTweets";
+import { Analytics } from "@vercel/analytics/react";
+import Head from "next/head";
 
 // ツイートの型定義
 type Tweet = {
@@ -160,187 +162,216 @@ const TwitterArchiveViewer = () => {
   };
 
   return (
-    <div className="container mx-auto p-4 ">
-      <div className="text-5xl mb-6 font-light">
-        Multi Twitter Archive Viewer
-        <span>
-          <a
-            href="https://twitter.com/shiryu_dev"
-            className="text-xl ml-8 text-blue-500 hover:underline"
-          >
-            作者(Twitter)
-          </a>
-        </span>
-      </div>
-      <div className="text-xl font-bold mb-6">
-        <Link href="https://github.com/Shiryu-Toujima-1f10210346/MultiTwiArchiveViewer#twittwer%E3%81%AE%E3%82%A2%E3%83%BC%E3%82%AB%E3%82%A4%E3%83%96dl%E6%A9%9F%E8%83%BD%E3%82%92%E7%94%A8%E3%81%84%E3%81%9F%E9%81%8E%E5%8E%BB%E3%83%84%E3%82%A4%E3%83%BC%E3%83%88%E3%83%93%E3%83%A5%E3%83%BC%E3%83%AF%E3%83%BC">
-          <p className="text-blue-500 hover:underline">使い方</p>
-        </Link>
-      </div>
-      {/* ユーザー入力フィールド */}
-      {userInputs.map((input, index) => (
-        <div key={index} className="mb-4">
-          <input
-            type="file"
-            accept=".js"
-            className="border p-2 rounded"
-            onChange={(e) =>
-              handleUserInputChange(
-                index,
-                e.target.files ? e.target.files[0] : null,
-                input.userName
-              )
-            }
-          />
-          <input
-            type="text"
-            placeholder={`User ${index + 1}'s Name`}
-            className="border p-2 rounded ml-2"
-            value={input.userName}
-            onChange={(e) =>
-              handleUserInputChange(index, input.file, e.target.value)
-            }
-          />
-          <button
-            onClick={() => removeUserInput(index)}
-            className="ml-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-          >
-            -
-          </button>
+    <>
+      <Head>
+        <title>tweet.js Viewer</title>
+        <meta
+          name="description"
+          content="Twitterのアーカイブを複数同時に表示することができるサービスです。"
+        />
+        <meta property="og:title" content="Multi Twitter Archive Viewer" />
+        <meta
+          property="og:description"
+          content="Twitterのアーカイブを複数同時に表示することができるサービスです。"
+        />
+        <meta
+          name="keywords"
+          content="Twitter,アーカイブ,ツイート,Tweet,tweet.js,tweets.js,X,"
+        />
+        <meta property="og:site_name" content="Multi Twitter Archive Viewer" />
+      </Head>
+      <div className="container mx-auto p-4">
+        <div className="text-5xl mb-6 font-light  tracking-widest">
+          M<span className="text-3xl">ulti </span>T
+          <span className="text-3xl">witter </span>A
+          <span className="text-3xl">rchive </span>V
+          <span className="text-3xl">iewer</span>
+          <span className="tracking-normal">
+            <a
+              href="https://twitter.com/shiryu_dev"
+              className="text-xl ml-8 text-blue-500 hover:underline"
+            >
+              作者(Twitter)
+            </a>
+          </span>
         </div>
-      ))}
-
-      {/* ユーザー入力フィールド追加ボタン */}
-      <button
-        onClick={addUserInput}
-        className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
-      >
-        ＋
-      </button>
-
-      {/* 検索バー */}
-      <div className="mb-4">
-        <input
-          type="text"
-          placeholder="ツイート検索..."
-          className="border p-2 rounded w-full"
-          value={searchQuery}
-          onChange={handleSearchChange}
-        />
-      </div>
-
-      {/* ツイートを表示するボタン */}
-      <div
-        className="flex 
-      justify-start 
-      md:flex-row gap-36"
-      >
-        <button
-          onClick={displayTweets}
-          className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-4 mb-4"
-        >
-          表示
-        </button>
-        {/* ツイート時間を降順にする */}
-
-        <button
-          onClick={() => {
-            clearDB();
-            clearTweets();
-          }}
-          className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-4 mb-4"
-        >
-          初期化
-        </button>
-      </div>
-
-      {/* 期間選択フィールド */}
-      <div className="mb-4">
-        <input
-          type="date"
-          className="border p-2 rounded mr-2"
-          value={dateRange.start}
-          onChange={(e) =>
-            setDateRange({ ...dateRange, start: e.target.value })
-          }
-        />
-        から
-        <input
-          type="date"
-          className="border p-2 rounded mr-2 ml-2"
-          value={dateRange.end}
-          onChange={(e) => setDateRange({ ...dateRange, end: e.target.value })}
-        />
-        まで
-        <button
-          onClick={filterTweetsByDateRange}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2"
-        >
-          期間を設定
-        </button>
-        <button
-          onClick={resetFilter}
-          className="ml-2 bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
-        >
-          リセット
-        </button>
-      </div>
-      <div>
-        <button
-          onClick={() => {
-            const sortedTweets = [...tweets];
-            sortedTweets.sort((a, b) => {
-              return (
-                new Date(b.created_at).getTime() -
-                new Date(a.created_at).getTime()
-              );
-            });
-            setTweets(sortedTweets);
-          }}
-          className="hover:bg-gray-200 font-bold py-2 px-4 rounded mt-4 mb-4 underline underline-thickness: 2 underline-offset-8"
-        >
-          新しい順
-        </button>
-
-        {/* ツイート時間を昇順にする */}
-        <button
-          onClick={() => {
-            const sortedTweets = [...tweets];
-            sortedTweets.sort((a, b) => {
-              return (
-                new Date(a.created_at).getTime() -
-                new Date(b.created_at).getTime()
-              );
-            });
-            setTweets(sortedTweets);
-          }}
-          className="hover:bg-gray-200 font-bold py-2 px-4 rounded mt-4 mb-4 underline underline-thickness: 2 underline-offset-8"
-        >
-          古い順
-        </button>
-      </div>
-      {/* ツイートの表示 */}
-      <div id="timeline">
-        {displayedTweets.map((tweet, index) => (
-          <div
-            key={index}
-            className="tweet p-4 border-b"
-            ref={
-              index === displayedTweets.length - 1 ? lastTweetElementRef : null
-            }
-          >
-            <strong className="font-bold">{tweet.userName}</strong>
-            <br />
-            <span className="text-sm text-gray-600">{tweet.created_at}</span>
-            <br />
-            <div className="mt-2">
-              {tweet.full_text}
-              <div className="media">{renderMedia(tweet)}</div>
-            </div>
+        <div className="text-xl font-bold mb-6">
+          <Link href="https://github.com/Shiryu-Toujima-1f10210346/MultiTwiArchiveViewer#twittwer%E3%81%AE%E3%82%A2%E3%83%BC%E3%82%AB%E3%82%A4%E3%83%96dl%E6%A9%9F%E8%83%BD%E3%82%92%E7%94%A8%E3%81%84%E3%81%9F%E9%81%8E%E5%8E%BB%E3%83%84%E3%82%A4%E3%83%BC%E3%83%88%E3%83%93%E3%83%A5%E3%83%BC%E3%83%AF%E3%83%BC">
+            <p className="text-blue-500 hover:underline">使い方(Github)</p>
+          </Link>
+        </div>
+        {/* ユーザー入力フィールド */}
+        {userInputs.map((input, index) => (
+          <div key={index} className="mb-4">
+            <input
+              type="file"
+              accept=".js"
+              className="border p-2 rounded"
+              onChange={(e) =>
+                handleUserInputChange(
+                  index,
+                  e.target.files ? e.target.files[0] : null,
+                  input.userName
+                )
+              }
+            />
+            <input
+              type="text"
+              placeholder={`User ${index + 1}'s Name`}
+              className="border p-2 rounded ml-2"
+              value={input.userName}
+              onChange={(e) =>
+                handleUserInputChange(index, input.file, e.target.value)
+              }
+            />
+            <button
+              onClick={() => removeUserInput(index)}
+              className="ml-2 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            >
+              -
+            </button>
           </div>
         ))}
+
+        {/* ユーザー入力フィールド追加ボタン */}
+        <button
+          onClick={addUserInput}
+          className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mb-4"
+        >
+          ＋
+        </button>
+
+        {/* 検索バー */}
+        <div className="mb-4">
+          <input
+            type="text"
+            placeholder="ツイート検索..."
+            className="border p-2 rounded w-full"
+            value={searchQuery}
+            onChange={handleSearchChange}
+          />
+        </div>
+
+        {/* ツイートを表示するボタン */}
+        <div
+          className="flex 
+      justify-start 
+      md:flex-row gap-36"
+        >
+          <button
+            onClick={displayTweets}
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mt-4 mb-4"
+          >
+            表示
+          </button>
+          {/* ツイート時間を降順にする */}
+
+          <button
+            onClick={() => {
+              if (confirm("ツイートを初期化しますか？")) {
+                clearDB();
+                clearTweets();
+              }
+            }}
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-4 mb-4"
+          >
+            初期化
+          </button>
+        </div>
+
+        {/* 期間選択フィールド */}
+        <div className="mb-4">
+          <input
+            type="date"
+            className="border p-2 rounded mr-2"
+            value={dateRange.start}
+            onChange={(e) =>
+              setDateRange({ ...dateRange, start: e.target.value })
+            }
+          />
+          から
+          <input
+            type="date"
+            className="border p-2 rounded mr-2 ml-2"
+            value={dateRange.end}
+            onChange={(e) =>
+              setDateRange({ ...dateRange, end: e.target.value })
+            }
+          />
+          まで
+          <button
+            onClick={filterTweetsByDateRange}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded ml-2"
+          >
+            期間を設定
+          </button>
+          <button
+            onClick={resetFilter}
+            className="ml-2 bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+          >
+            リセット
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={() => {
+              const sortedTweets = [...tweets];
+              sortedTweets.sort((a, b) => {
+                return (
+                  new Date(b.created_at).getTime() -
+                  new Date(a.created_at).getTime()
+                );
+              });
+              setTweets(sortedTweets);
+            }}
+            className="hover:bg-gray-200 font-bold py-2 px-4 rounded mt-4 mb-4 underline underline-thickness: 2 underline-offset-8"
+          >
+            新しい順
+          </button>
+
+          {/* ツイート時間を昇順にする */}
+          <button
+            onClick={() => {
+              const sortedTweets = [...tweets];
+              sortedTweets.sort((a, b) => {
+                return (
+                  new Date(a.created_at).getTime() -
+                  new Date(b.created_at).getTime()
+                );
+              });
+              setTweets(sortedTweets);
+            }}
+            className="hover:bg-gray-200 font-bold py-2 px-4 rounded mt-4 mb-4 underline underline-thickness: 2 underline-offset-8"
+          >
+            古い順
+          </button>
+        </div>
+        {/* ツイートの表示 */}
+        <div id="timeline">
+          {displayedTweets.map((tweet, index) => (
+            <div
+              key={index}
+              className="tweet p-4 border-b"
+              ref={
+                index === displayedTweets.length - 1
+                  ? lastTweetElementRef
+                  : null
+              }
+            >
+              <strong className="font-bold">{tweet.userName}</strong>
+              <br />
+              <span className="text-sm text-gray-600">{tweet.created_at}</span>
+              <br />
+              <div className="mt-2">
+                {tweet.full_text}
+                <div className="media">{renderMedia(tweet)}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+        <Analytics />
       </div>
-    </div>
+    </>
   );
 };
 
